7e635fae23eeb01ff57a5e5c4938b271514741a5
hansehe <hans.erik.heggem@gmail.com>
2020-10-06 15:58:16 +0200

    wip - fixing websockets

---
+++ src/src/index-old.ts
@@ -0,0 +1,43 @@
+import { GraphQLServer, Options } from 'graphql-yoga'
+import { getRemoteSchemas } from "./remote-schema/";
+import { getENV, getENVArray } from "./env";
+
+const start = async () => {
+
+    const uris = getENVArray("GRAPHQL_URL");
+    const schema = await getRemoteSchemas(uris).catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
+
+    const server = new GraphQLServer({
+        schema,
+    });
+
+    const activateUpdateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY", "true") === "true";
+    const updateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY_INTERVAL_MS", "60000");
+    if (activateUpdateGatewayInterval === true)
+    {
+        setInterval(async () => {
+        try {
+            const updatedSchema = await getRemoteSchemas(uris);
+            server.executableSchema = updatedSchema;
+        } catch (error) {
+            console.error(error);
+        }
+        }, updateGatewayInterval);
+    }
+
+    const HOST = getENV("HOST", "http://localhost");
+    const PORT = getENV("PORT", "4000");
+
+    const options: Options = {
+        port: PORT,
+        endpoint: '/graphql',
+        playground: '/graphql',
+    }
+
+    server.start(options, ({ port, endpoint }) => console.log(`Server is running on ${HOST}:${port}${endpoint}`))
+}
+
+start();
--- src/src/index.ts
+++ src/src/index.ts
@@ -1,43 +1,60 @@
-import { GraphQLServer, Options } from 'graphql-yoga'
+const express = require( "express" );
+import {
+  graphqlExpress,
+  graphiqlExpress,
+} from 'apollo-server-express';
+import { execute, subscribe } from 'graphql';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+
 import { getRemoteSchemas } from "./remote-schema/";
 import { getENV, getENVArray } from "./env";
+import bodyParser = require('body-parser');
 
 const start = async () => {
-
     const uris = getENVArray("GRAPHQL_URL");
     const schema = await getRemoteSchemas(uris).catch((e) => {
         console.error(e);
         process.exit(1);
     });
 
-    const server = new GraphQLServer({
-        schema,
+    const HOST: string = getENV("HOST", "http://localhost");
+    const PORT: string = getENV("PORT", "4000");
+    const server = express();
+
+    server.use('/graphql', bodyParser.json(), graphqlExpress({
+        schema
+    }));
+
+    let WS_HOST = HOST.replace('http:', 'ws:');
+    if (HOST.startsWith('https:')) {
+        WS_HOST = HOST.replace('https:', 'wss:');
+    }
+    
+    const endpointURL = '/graphql'
+    const subscriptionsEndpointUrl = '/graphql'
+    const subscriptionsEndpoint = `${WS_HOST}:${PORT}${subscriptionsEndpointUrl}`
+
+    server.use('/graphiql', graphiqlExpress({
+        endpointURL: endpointURL,
+        subscriptionsEndpoint: subscriptionsEndpoint
+    }));
+
+    // Wrap the Express server
+    const ws = createServer(server);
+    ws.listen(PORT, () => {
+        console.log(`Apollo Server is now running on ${HOST}:${PORT}${endpointURL}`);
+        console.log(`Apollo Server subscriptions is now running on ${subscriptionsEndpoint}`);
+        // Set up the WebSocket for handling GraphQL subscriptions
+        new SubscriptionServer({
+            execute,
+            subscribe,
+            schema
+        }, {
+                server: ws,
+                path: subscriptionsEndpointUrl,
+            });
     });
-
-    const activateUpdateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY", "true") === "true";
-    const updateGatewayInterval = getENV("GRAPHQL_UPDATE_GATEWAY_INTERVAL_MS", "60000");
-    if (activateUpdateGatewayInterval === true)
-    {
-        setInterval(async () => {
-        try {
-            const updatedSchema = await getRemoteSchemas(uris);
-            server.executableSchema = updatedSchema;
-        } catch (error) {
-            console.error(error);
-        }
-        }, updateGatewayInterval);
-    }
-
-    const HOST = getENV("HOST", "http://localhost");
-    const PORT = getENV("PORT", "4000");
-
-    const options: Options = {
-        port: PORT,
-        endpoint: '/graphql',
-        playground: '/graphql',
-    }
-
-    server.start(options, ({ port, endpoint }) => console.log(`Server is running on ${HOST}:${port}${endpoint}`))
 }
 
 start();
