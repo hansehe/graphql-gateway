0f2c93099d6628070053865dcb70db48f165879b
Hans Erik Heggem <hanserik@neate.no>
2022-08-11 16:26:45 +0200

    minor bug fix

--- GraphqlTemplate/PyTemplate/models/RandomModel.py
+++ GraphqlTemplate/PyTemplate/models/RandomModel.py
@@ -7,5 +7,6 @@ from PyTemplate.models.BaseModel import BaseModel
 class RandomModel(BaseModel):
     def __init__(self, **kwargs):
         kwargs.setdefault('number', random.random())
+        super().__init__(**kwargs)
 
     number: Optional[float] = None
