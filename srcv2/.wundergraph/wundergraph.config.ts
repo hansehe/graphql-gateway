e4c1eca6228dfcd0ed224b03fda83e02cf90a6da
Hans Erik Heggem <hanserik@neate.no>
2024-08-06 16:01:47 +0200

    wip - working solution

--- srcv2/.wundergraph/wundergraph.config.ts
+++ srcv2/.wundergraph/wundergraph.config.ts
@@ -3,63 +3,45 @@ import {
   cors,
   EnvironmentVariable,
   introspect,
+  LoggerLevel,
   templates,
 } from "@wundergraph/sdk";
 import server from "./wundergraph.server";
 import operations from "./wundergraph.operations";
 
 const graphqlServices = [];
-const spaceX = introspect.graphql({
-  apiNamespace: "spacex",
-  url: "https://spacex-api.fly.dev/graphql",
-});
 const account1 = introspect.graphql({
   apiNamespace: "account1",
-  url: "http://localhost:5001/graphql",
+  url: "http://host.docker.internal:5001/graphql",
+  introspection: {
+    pollingIntervalSeconds: 30,
+  },
 });
 const account2 = introspect.graphql({
   apiNamespace: "account2",
-  url: "http://localhost:5002/graphql",
+  url: "http://host.docker.internal:5002/graphql",
+  introspection: {
+    pollingIntervalSeconds: 30,
+  },
 });
 
-graphqlServices.push(spaceX);
 graphqlServices.push(account1);
 graphqlServices.push(account2);
 
-// configureWunderGraph emits the configuration
 configureWunderGraphApplication({
   apis: graphqlServices,
   server,
   operations,
-  codeGenerators: [
-    {
-      templates: [
-        // use all the typescript react templates to generate a client
-        ...templates.typescript.all,
-      ],
-      // create-react-app expects all code to be inside /src
-      // path: "../frontend/src/generated",
-    },
-  ],
-  cors: {
-    ...cors.allowAll,
-    allowedOrigins:
-      process.env.NODE_ENV === "production"
-        ? [
-            // change this before deploying to production to the actual domain where you're deploying your app
-            "http://localhost:3000",
-          ]
-        : [
-            "http://localhost:3000",
-            new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
-          ],
-  },
-  dotGraphQLConfig: {
-    hasDotWunderGraphDirectory: false,
-  },
   security: {
-    enableGraphQLEndpoint:
-      process.env.NODE_ENV !== "production" ||
-      process.env.GITPOD_WORKSPACE_ID !== undefined,
+    enableGraphQLEndpoint: true,
+  },
+  options: {
+    listen: {
+      host: new EnvironmentVariable('WG_NODE_HOST', 'localhost'),
+      port: new EnvironmentVariable('WG_NODE_PORT', '8181'),
+    },
+    logger: {
+      level: new EnvironmentVariable<LoggerLevel>('WG_LOG_LEVEL', 'info'),
+    },
   },
 });
